b1315f1115dad1cfc0c52f4623ac02c09cdc8456
Kai van Lopik <K.vanLopik@cs.ru.nl>
2025-10-02 17:53:48 +0200

    Updated calling code for changed API (should have been included in an earlier commit).

--- cpp/pep/client/Client.TokenBlock.cpp
+++ cpp/pep/client/Client.TokenBlock.cpp
@@ -6,16 +6,15 @@ namespace pep {
 rxcpp::observable<TokenBlockingCreateResponse> Client::tokenBlockCreate(
     tokenBlocking::TokenIdentifier target,
     std::string note) {
-  return clientKeyServer->sendRequest<TokenBlockingCreateResponse>(
-      sign(TokenBlockingCreateRequest{std::move(target), std::move(note)}));
+  return clientKeyServer->requestTokenBlockingCreate(TokenBlockingCreateRequest{std::move(target), std::move(note)});
 }
 
 rxcpp::observable<TokenBlockingRemoveResponse> Client::tokenBlockRemove(int64_t id) {
-  return clientKeyServer->sendRequest<TokenBlockingRemoveResponse>(sign(TokenBlockingRemoveRequest{id}));
+  return clientKeyServer->requestTokenBlockingRemove(TokenBlockingRemoveRequest{id});
 }
 
 rxcpp::observable<TokenBlockingListResponse> Client::tokenBlockList() {
-  return clientKeyServer->sendRequest<TokenBlockingListResponse>(sign(TokenBlockingListRequest{}));
+  return clientKeyServer->requestTokenBlockingList();
 }
 
 } // namespace pep
