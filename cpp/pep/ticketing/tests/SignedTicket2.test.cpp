df25e75b47a72a7db7af417dfa863bc12d57efaf
Kai van Lopik <K.vanLopik@cs.ru.nl>
2025-10-09 15:03:30 +0200

    Improved comments.

--- cpp/pep/ticketing/tests/SignedTicket2.test.cpp
+++ cpp/pep/ticketing/tests/SignedTicket2.test.cpp
@@ -20,8 +20,8 @@ TEST(SignedTicket2, ClassesHaveBackwardCompatibleProperties) {
   using namespace pep;
 
   /* SignedTicket2 and SignedTicketRequest2 used to be standalone classes, making them inconvenient for templated
-   * processing. E.g. when generic message signing was added to the Client class, we needed a separate overload
-   * for the unrelated SignedTicketRequest2 class:
+   * processing. E.g. when generic message signing was added, we needed a separate overload because SignedTicketRequest2
+   * wasn't a specialization of Signed<T>:
    * 
    * class Client {
    *   template <typename T> Signed<T> sign(T message) { ... }   // What we wanted to add
@@ -30,9 +30,11 @@ TEST(SignedTicket2, ClassesHaveBackwardCompatibleProperties) {
    *
    * See https://gitlab.pep.cs.ru.nl/pep/core/-/commit/c86cca366e023322a6cf2895362fd6562e280ab2#635ab8e17c7435817d081539435759a664921159_779_789
    * 
-   * To allow all SignedXyz classes to be processed by unspecialized templated code, the SignedTicket2 and
-   * SignedTicketRequest2 classes were later turned into aliases for (explicit specializations of) the corresponding
-   * Signed<T> templates. This unit test verifies that the aliases behave the same as the original classes.
+   * To correct this, the SignedTicket2 and SignedTicketRequest2 classes were later turned into aliases for
+   * (explicit specializations of) the corresponding Signed<T> templates. This unit test verifies that the aliases
+   * are compatible with the original classes, e.g. so that previously serialized (and possibly stored) SignedTicket2
+   * instances can be deserialized to the new Signed<Ticket2> type.
+   * 
    * The expected names and MessageMagic values were extracted from the original classes in commit d9e9581.
    */
   VerifyBackwardCompatibleProperties<SignedTicket2>("SignedTicket2", 3936116042);
