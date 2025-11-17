f5c872964203192d7c2918088cd4828603f61a3e
Kai van Lopik <K.vanLopik@cs.ru.nl>
2025-11-17 17:09:59 +0100

    Unit test now also checks for duplicate user group(set content)s.

--- cpp/pep/auth/tests/ServerTraits.test.cpp
+++ cpp/pep/auth/tests/ServerTraits.test.cpp
@@ -1,73 +1,98 @@
 #include <pep/auth/ServerTraits.hpp>
 #include <pep/utils/MiscUtil.hpp>
+#include <boost/algorithm/string/join.hpp>
 #include <gtest/gtest.h>
 
 namespace {
 
 // Not using a map<> or unordered_map<> because we'd need to use some (possibly duplicate) property as the key
 template <typename T> using ServerProperty = std::pair<std::string, T>;
-template <typename T> using ServerProperties = std::vector<ServerProperty<T>>;
-
-template <typename TDestination, typename TSource>
-ServerProperties<TDestination> ConvertServerProperties(const ServerProperties<TSource>& source, const std::function<TDestination(const TSource&)>& convertValue) {
-  ServerProperties<TDestination> result;
-  result.reserve(source.size());
-  std::transform(source.begin(), source.end(), std::back_inserter(result), [convertValue](const ServerProperty<TSource>& property) {
-    return std::make_pair(property.first, convertValue(property.second));
-    });
-  return result;
-}
 
 template <typename T, typename Enable = void>
-struct UniqueVerification;
+struct ServerPropertyValue;
+
+template <typename T>
+struct BasicServerPropertyValue {
+  static bool IsEmpty(const T&) { return false; }
+  static std::optional<std::string> DescribeIfDuplicate(const T& lhs, const T& rhs) {
+    if (lhs == rhs) {
+      return ServerPropertyValue<T>::ToString(lhs);
+    }
+    return std::nullopt;
+  }
+};
 
 template <>
-struct UniqueVerification<std::string> {
-  static void Perform(const std::string& description, ServerProperties<std::string> properties) {
-    auto end = properties.end();
-    for (auto i = properties.begin(); i != end; ++i) {
-      auto j = i;
-      for (++j; j != end; ++j) {
-        EXPECT_NE(i->second, j->second) << i->first << " and " << j->first << " have the same \"" << description << "\" value of \"" << i->second << '"';
+struct ServerPropertyValue<std::string> : public BasicServerPropertyValue<std::string> {
+  static std::string ToString(const std::string& value) { return '"' + value + '"'; }
+};
+
+template <typename T>
+struct ServerPropertyValue<T, std::enable_if_t<std::is_enum_v<T>>> : public BasicServerPropertyValue<T> {
+  static std::string ToString(const T& value) { return std::to_string(pep::ToUnderlying(value)); }
+};
+
+template <typename T>
+struct ServerPropertyValue<std::optional<T>> : public BasicServerPropertyValue<std::optional<T>> {
+  static bool IsEmpty(const std::optional<T>& value) { return !value.has_value(); }
+  static std::string ToString(const std::optional<T>& value) {
+    assert(value.has_value());
+    return ServerPropertyValue<T>::ToString(*value);
+  }
+};
+
+template <typename T>
+struct ServerPropertyValue<std::unordered_set<T>> {
+  static bool IsEmpty(const std::unordered_set<T>& value) { return value.empty(); }
+  static std::optional<std::string> DescribeIfDuplicate(const std::unordered_set<T>& lhs, const std::unordered_set<T>& rhs) {
+    // Collect (descriptions of) duplicate entries
+    std::vector<std::string> entries;
+    for (const auto& i : lhs) {
+      for (const auto& j : rhs) {
+        if (auto duplicate = ServerPropertyValue<T>::DescribeIfDuplicate(i, j)) {
+          entries.emplace_back(*duplicate);
+        }
       }
     }
-  }
-};
 
-template <typename T>
-struct UniqueVerification<T, std::enable_if_t<std::is_enum_v<T>>> {
-  static void Perform(const std::string& description, ServerProperties<T> properties) {
-    auto stringified = ConvertServerProperties<std::string, T>(properties, [](const T& value) {return std::to_string(pep::ToUnderlying(value)); });
-    return UniqueVerification<std::string>::Perform(description, std::move(stringified));
-  }
-};
-
-template <typename T>
-struct UniqueVerification<std::optional<T>> {
-  static void Perform(const std::string& description, ServerProperties<std::optional<T>> properties) {
-    auto removed = std::remove_if(properties.begin(), properties.end(), [](const ServerProperty<std::optional<T>>& property) {
-      return !property.second.has_value();
-      });
-    properties.erase(removed, properties.end());
-    auto nonnull = ConvertServerProperties<T, std::optional<T>>(properties, [](const std::optional<T>& optional) -> T {return *optional; });
-    return UniqueVerification<T>::Perform(description, std::move(nonnull));
+    if (entries.empty()) {
+      return std::nullopt;
+    }
+    return "{ " + boost::join(entries, ", ") + " }";
   }
 };
 
 template <typename T>
 void VerifyServerMethodProducesUniqueProperties(const std::vector<pep::ServerTraits>& servers, const std::string& property, T (pep::ServerTraits::*method)() const) {
+  // Aggregate the properties for all servers into a vector
   using Plain = std::remove_const_t<std::remove_reference_t<T>>;
-  ServerProperties<Plain> properties;
+  std::vector<ServerProperty<Plain>> properties;
   std::transform(servers.begin(), servers.end(), std::back_inserter(properties), [method](const pep::ServerTraits& server) {
     return std::make_pair(server.description(), (server.*method)());
     });
-  UniqueVerification<Plain>::Perform(property, std::move(properties));
+
+  // Compare each server('s property) against each other server('s property)
+  using Value = ServerPropertyValue<Plain>;
+  auto end = properties.end();
+  for (auto i = properties.begin(); i != end; ++i) {
+    if (!Value::IsEmpty(i->second)) { // Don't compare e.g. nullopt
+      auto j = i;
+      for (++j; j != end; ++j) {
+        if (!Value::IsEmpty(j->second)) { // Don't compare e.g. nullopt
+          if (std::optional<std::string> duplicate = Value::DescribeIfDuplicate(i->second, j->second)) {
+            FAIL() << i->first << " and " << j->first << " have duplicate " << property << ' ' << *duplicate;
+          }
+        }
+      }
+    }
+  }
 }
 
 }
 
 TEST(ServerTraits, HaveUniqueProperties) {
   auto servers = pep::ServerTraits::All();
+  servers.emplace_back(pep::ServerTraits::AuthServer());
 
   VerifyServerMethodProducesUniqueProperties(servers, "abbreviation", &pep::ServerTraits::abbreviation);
   VerifyServerMethodProducesUniqueProperties(servers, "description", &pep::ServerTraits::description);
@@ -76,7 +101,7 @@ TEST(ServerTraits, HaveUniqueProperties) {
   VerifyServerMethodProducesUniqueProperties(servers, "TLS certificate subject", &pep::ServerTraits::tlsCertificateSubject);
 
   VerifyServerMethodProducesUniqueProperties(servers, "user group", &pep::ServerTraits::userGroup);
-  // TODO: VerifyServerMethodProducesUniqueProperties(servers, "user groups", &pep::ServerTraits::userGroups);
+  VerifyServerMethodProducesUniqueProperties(servers, "user groups", &pep::ServerTraits::userGroups);
   VerifyServerMethodProducesUniqueProperties(servers, "enrolls as", &pep::ServerTraits::enrollsAs);
   VerifyServerMethodProducesUniqueProperties(servers, "enrollment subject", &pep::ServerTraits::enrollmentSubject);
 }
