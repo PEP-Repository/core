6115c95aa9b37094cf5c2cb746da186d91f7b8e5
Linus van Elswijk <l.vanelswijk@science.ru.nl>
2025-12-03 16:34:33 +0100

    test(EntryPayload): Test comparison operators
    move tests to own file

---
+++ cpp/pep/storagefacility/tests/EntryPayload.test.cpp
@@ -0,0 +1,81 @@
+#include <memory>
+
+#include <pep/storagefacility/EntryPayload.hpp>
+#include <pep/storagefacility/PersistedEntryProperties.hpp>
+
+#include <gtest/gtest.h>
+
+using pep::EntryPayload;
+
+TEST(EntryPayload, EntryPayloadEquality) {
+    using namespace pep;
+    using BasePtr = std::shared_ptr<pep::EntryPayload>;
+
+    { // both InlinedEntryPayload objects
+      const BasePtr lhs = std::make_shared<pep::InlinedEntryPayload>("1 2 3 4 5 6", 11);
+      const BasePtr same = std::make_shared<pep::InlinedEntryPayload>("1 2 3 4 5 6", 11);
+      const BasePtr different_data = std::make_shared<pep::InlinedEntryPayload>("1 2 ZZZ 5 6", 11);
+      const BasePtr different_size = std::make_shared<pep::InlinedEntryPayload>("1 2 3 4 5 6", 7);
+
+      EXPECT_EQ(*lhs, *same);
+      EXPECT_EQ(*lhs, *lhs); // edge_case
+      EXPECT_NE(*lhs, *different_data);
+      EXPECT_NE(*lhs, *different_size);
+    }
+    { // both PagedEntryPayload objects
+      const auto lhs_props = [] {
+        PersistedEntryProperties props;
+        SetPersistedEntryProperty(props, "filesize", uint64_t{1024});
+        SetPersistedEntryProperty(props, "pagesize", uint64_t{512});
+        return props;
+      };
+      const auto diff_filesize_props = [] {
+        PersistedEntryProperties props;
+        SetPersistedEntryProperty(props, "filesize", uint64_t{2048});
+        SetPersistedEntryProperty(props, "pagesize", uint64_t{512});
+        return props;
+      };
+      const auto diff_pagesize_props = [] {
+        PersistedEntryProperties props;
+        SetPersistedEntryProperty(props, "filesize", uint64_t{1024});
+        SetPersistedEntryProperty(props, "pagesize", uint64_t{256});
+        return props;
+      };
+      const auto lhs_pages = std::vector<PageId>{0xDEADBEEF};
+      const auto diff_pages = std::vector<PageId>{0x12345678};
+
+      auto props = lhs_props();
+      const BasePtr lhs = std::make_shared<pep::PagedEntryPayload>(props, lhs_pages); // constructor consumes props
+      props = lhs_props();
+      const BasePtr same = std::make_shared<pep::PagedEntryPayload>(props, lhs_pages);
+      props = diff_filesize_props();
+      const BasePtr diff1 = std::make_shared<pep::PagedEntryPayload>(props, lhs_pages);
+      props = diff_pagesize_props();
+      const BasePtr diff2 = std::make_shared<pep::PagedEntryPayload>(props, lhs_pages);
+      props = lhs_props();
+      const BasePtr diff3 = std::make_shared<pep::PagedEntryPayload>(props, diff_pages);
+
+      EXPECT_EQ(*lhs, *same);
+      EXPECT_EQ(*lhs, *lhs); // edge_case
+      EXPECT_NE(*lhs, *diff1);
+      EXPECT_NE(*lhs, *diff2);
+      EXPECT_NE(*lhs, *diff3);
+    }
+    { // different object types
+      const auto lhs_props = [] {
+        PersistedEntryProperties props;
+        SetPersistedEntryProperty(props, "filesize", uint64_t{11});
+        SetPersistedEntryProperty(props, "pagesize", uint64_t{11});
+        return props;
+      };
+      const auto lhs_pages = std::vector<PageId>{0xDEADBEEF};
+
+      auto props = lhs_props();
+      const BasePtr paged = std::make_shared<pep::PagedEntryPayload>(props, lhs_pages); // constructor consumes props
+      const BasePtr inlined = std::make_shared<pep::InlinedEntryPayload>("1 2 3 4 5 6", 11);
+
+      EXPECT_NE(*paged, *inlined);
+      EXPECT_NE(*inlined, *paged);
+    }
+}
+
