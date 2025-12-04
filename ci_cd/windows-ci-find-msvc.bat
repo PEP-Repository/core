022ea34e14731c2815cf1533cbe437fe7dc32aaf
Steven Wallis de Vries <swdv@cs.ru.nl>
2025-12-04 14:35:48 +0100

    Clarify windows-ci-find-msvc.bat wrapper note

--- ci_cd/windows-ci-find-msvc.bat
+++ ci_cd/windows-ci-find-msvc.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM Wrapper to forward environment variables until everything is PowerShell
+REM Invoke PowerShell script, then pick up / copy any environment variables that it set (especially PEP_VCVARS_BAT)
 for /f "tokens=*" %%i in ('pwsh -ExecutionPolicy Bypass -File "%~dp0\..\scripts\wrap-echo-cmd-envvars.ps1" ^
   -ScriptPath "%~dp0\windows-ci-find-msvc.ps1" ^
   PEP_VISUAL_STUDIO_DIR PEP_VCVARS_BAT') do (
