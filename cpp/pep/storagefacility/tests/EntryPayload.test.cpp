aec6846b141dcfb09d470a0e568b1f6a878e3ff3
Linus van Elswijk <l.vanelswijk@science.ru.nl>
2025-12-09 09:21:23 +0100

    test(EntryPayload): Add helper for persisted properties

--- cpp/pep/storagefacility/tests/EntryPayload.test.cpp
+++ cpp/pep/storagefacility/tests/EntryPayload.test.cpp
@@ -17,6 +17,20 @@ std::shared_ptr<EntryPayload> MakeEntryPayload(Args&&... args) {
   return std::make_shared<T>(std::forward<Args>(args)...);
 }
 
+/// Record type of (only) entry properties that we use during tests
+struct EntryProperties final {
+  uint64_t filesize;
+  uint64_t pagesize;
+};
+
+/// Convenience function to create PersistedEntryProperties objects for tests
+PersistedEntryProperties AsPersistentEntryProperties(const EntryProperties vals) {
+  PersistedEntryProperties persistedProps;
+  SetPersistedEntryProperty(persistedProps, "filesize", vals.filesize);
+  SetPersistedEntryProperty(persistedProps, "pagesize", vals.filesize);
+  return persistedProps;
+}
+
 } // namespace
 
 TEST(EntryPayload, default_constructed_paged_payload_is_empty) {
@@ -29,9 +43,7 @@ TEST(EntryPayload, default_constructed_paged_payload_is_empty) {
 TEST(EntryPayload, payloads_are_not_strictly_equal_if_types_are_different) {
   const auto pageIds = std::vector<PageId>{12, 13, 14};
 
-  PersistedEntryProperties props;
-  SetPersistedEntryProperty(props, "filesize", uint64_t{11});
-  SetPersistedEntryProperty(props, "pagesize", uint64_t{11});
+  auto props = AsPersistentEntryProperties({.filesize = 11, .pagesize = 11});
   const auto paged = MakeEntryPayload<PagedEntryPayload>(props, std::vector<PageId>{12, 13, 14});
   const auto inlined = MakeEntryPayload<InlinedEntryPayload>("1 2 3 4 5 6", 11);
 
@@ -61,29 +73,21 @@ TEST(EntryPayload, inlined_payloads_are_not_strictly_equal_if_their_contents_dif
 }
 
 TEST(EntryPayload, paged_payloads_are_strictly_equal_if_their_content_is_equal) {
-  const auto set_props_A = [](PersistedEntryProperties& props) {
-    SetPersistedEntryProperty(props, "filesize", uint64_t{1024});
-    SetPersistedEntryProperty(props, "pagesize", uint64_t{512});
-  };
-  const auto set_props_B = [](PersistedEntryProperties& props) {
-    SetPersistedEntryProperty(props, "filesize", uint64_t{2048});
-    SetPersistedEntryProperty(props, "pagesize", uint64_t{512});
-  };
+  const auto propsA = EntryProperties{.filesize = 1024, .pagesize = 512};
+  const auto propsB = EntryProperties{.filesize = 2048, .pagesize = 512};
   const auto pages_A = std::vector<PageId>{12};
   const auto pages_B = std::vector<PageId>{35, 6};
 
-  PersistedEntryProperties props;
-
-  set_props_A(props);
+  auto props = AsPersistentEntryProperties(propsA);
   const auto paged_AA_0 = MakeEntryPayload<PagedEntryPayload>(props, pages_A); // constructor consumes props
 
-  set_props_A(props);
+  props = AsPersistentEntryProperties(propsA);
   const auto paged_AA_1 = MakeEntryPayload<PagedEntryPayload>(props, pages_A);
 
-  set_props_B(props);
+  props = AsPersistentEntryProperties(propsB);
   const auto paged_BB_0 = MakeEntryPayload<PagedEntryPayload>(props, pages_B);
 
-  set_props_B(props);
+  props = AsPersistentEntryProperties(propsB);
   const auto paged_BB_1 = MakeEntryPayload<PagedEntryPayload>(props, pages_B);
 
   EXPECT_TRUE(StrictlyEqual(*paged_AA_0, *paged_AA_1));
@@ -93,37 +97,28 @@ TEST(EntryPayload, paged_payloads_are_strictly_equal_if_their_content_is_equal) 
   EXPECT_TRUE(StrictlyEqual(*paged_AA_0, *paged_AA_0));
 
   // edge case: empty payloads are equal (regardless of how are constructed)
-  SetPersistedEntryProperty(props, "filesize", uint64_t{0});
-  SetPersistedEntryProperty(props, "pagesize", uint64_t{0});
+  props = AsPersistentEntryProperties({.filesize = 0, .pagesize = 0});
   const auto empty_0 = MakeEntryPayload<PagedEntryPayload>(props, std::vector<PageId>{});
   const auto empty_1 = std::make_shared<PagedEntryPayload>();
   EXPECT_TRUE(StrictlyEqual(*empty_0, *empty_1));
 }
 
 TEST(EntryPayload, paged_payloads_are_not_strictly_equal_if_their_contents_differ) {
-  const auto set_props_A = [](PersistedEntryProperties& props) {
-    SetPersistedEntryProperty(props, "filesize", uint64_t{1024});
-    SetPersistedEntryProperty(props, "pagesize", uint64_t{512});
-  };
-  const auto set_props_B = [](PersistedEntryProperties& props) {
-    SetPersistedEntryProperty(props, "filesize", uint64_t{2048});
-    SetPersistedEntryProperty(props, "pagesize", uint64_t{512});
-  };
+  const auto propsA = EntryProperties{.filesize = 1024, .pagesize = 512};
+  const auto propsB = EntryProperties{.filesize = 2048, .pagesize = 512};
   const auto pages_A = std::vector<PageId>{12};
   const auto pages_B = std::vector<PageId>{35, 6};
 
-  PersistedEntryProperties props;
-
-  set_props_A(props);
+  auto props = AsPersistentEntryProperties(propsA);
   const auto paged_AA = MakeEntryPayload<PagedEntryPayload>(props, pages_A); // constructor consumes props
 
-  set_props_A(props);
+  props = AsPersistentEntryProperties(propsA);
   const auto paged_AB = MakeEntryPayload<PagedEntryPayload>(props, pages_B);
 
-  set_props_B(props);
+  props = AsPersistentEntryProperties(propsB);
   const auto paged_BA = MakeEntryPayload<PagedEntryPayload>(props, pages_A);
 
-  set_props_B(props);
+  props = AsPersistentEntryProperties(propsB);
   const auto paged_BB = MakeEntryPayload<PagedEntryPayload>(props, pages_B);
 
   EXPECT_FALSE(StrictlyEqual(*paged_AA, *paged_AB));
