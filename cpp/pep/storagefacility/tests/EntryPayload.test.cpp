07e745333eb43ee40a334c4ddb4f1b6c594c74db
Linus van Elswijk <l.vanelswijk@science.ru.nl>
2025-12-09 09:42:10 +0100

    test(EntryPayload): consistently use camelCase for vars

--- cpp/pep/storagefacility/tests/EntryPayload.test.cpp
+++ cpp/pep/storagefacility/tests/EntryPayload.test.cpp
@@ -34,10 +34,10 @@ PersistedEntryProperties AsPersistentEntryProperties(const EntryProperties vals)
 } // namespace
 
 TEST(EntryPayload, default_constructed_paged_payload_is_empty) {
-  const auto default_constructed = std::make_shared<PagedEntryPayload>();
-  EXPECT_FALSE(default_constructed->pageSize().has_value());
-  EXPECT_EQ(default_constructed->pageCount(), 0);
-  EXPECT_EQ(default_constructed->size(), 0);
+  const auto defaultConstructed = std::make_shared<PagedEntryPayload>();
+  EXPECT_FALSE(defaultConstructed->pageSize().has_value());
+  EXPECT_EQ(defaultConstructed->pageCount(), 0);
+  EXPECT_EQ(defaultConstructed->size(), 0);
 }
 
 TEST(EntryPayload, payloads_are_not_strictly_equal_if_types_are_different) {
@@ -60,8 +60,8 @@ TEST(EntryPayload, inlined_payloads_are_strictly_equal_if_their_content_is_equal
       *MakeEntryPayload<InlinedEntryPayload>("3 4 5", 5)));
 
   // edge_case: compare to self
-  const auto specific_instance = MakeEntryPayload<InlinedEntryPayload>("ABCD", 4);
-  EXPECT_TRUE(StrictlyEqual(*specific_instance, *specific_instance));
+  const auto specificInstance = MakeEntryPayload<InlinedEntryPayload>("ABCD", 4);
+  EXPECT_TRUE(StrictlyEqual(*specificInstance, *specificInstance));
 }
 
 TEST(EntryPayload, inlined_payloads_are_not_strictly_equal_if_their_contents_differ) {
@@ -75,20 +75,20 @@ TEST(EntryPayload, inlined_payloads_are_not_strictly_equal_if_their_contents_dif
 TEST(EntryPayload, paged_payloads_are_strictly_equal_if_their_content_is_equal) {
   const auto propsA = EntryProperties{.filesize = 1024, .pagesize = 512};
   const auto propsB = EntryProperties{.filesize = 2048, .pagesize = 512};
-  const auto pages_A = std::vector<PageId>{12};
-  const auto pages_B = std::vector<PageId>{35, 6};
+  const auto pagesA = std::vector<PageId>{12};
+  const auto pagesB = std::vector<PageId>{35, 6};
 
   auto props = AsPersistentEntryProperties(propsA);
-  const auto paged_AA_0 = MakeEntryPayload<PagedEntryPayload>(props, pages_A); // constructor consumes props
+  const auto paged_AA_0 = MakeEntryPayload<PagedEntryPayload>(props, pagesA); // constructor consumes props
 
   props = AsPersistentEntryProperties(propsA);
-  const auto paged_AA_1 = MakeEntryPayload<PagedEntryPayload>(props, pages_A);
+  const auto paged_AA_1 = MakeEntryPayload<PagedEntryPayload>(props, pagesA);
 
   props = AsPersistentEntryProperties(propsB);
-  const auto paged_BB_0 = MakeEntryPayload<PagedEntryPayload>(props, pages_B);
+  const auto paged_BB_0 = MakeEntryPayload<PagedEntryPayload>(props, pagesB);
 
   props = AsPersistentEntryProperties(propsB);
-  const auto paged_BB_1 = MakeEntryPayload<PagedEntryPayload>(props, pages_B);
+  const auto paged_BB_1 = MakeEntryPayload<PagedEntryPayload>(props, pagesB);
 
   EXPECT_TRUE(StrictlyEqual(*paged_AA_0, *paged_AA_1));
   EXPECT_TRUE(StrictlyEqual(*paged_BB_0, *paged_BB_1));
@@ -98,28 +98,28 @@ TEST(EntryPayload, paged_payloads_are_strictly_equal_if_their_content_is_equal) 
 
   // edge case: empty payloads are equal (regardless of how are constructed)
   props = AsPersistentEntryProperties({.filesize = 0, .pagesize = 0});
-  const auto empty_0 = MakeEntryPayload<PagedEntryPayload>(props, std::vector<PageId>{});
-  const auto empty_1 = std::make_shared<PagedEntryPayload>();
-  EXPECT_TRUE(StrictlyEqual(*empty_0, *empty_1));
+  const auto emptyA = MakeEntryPayload<PagedEntryPayload>(props, std::vector<PageId>{});
+  const auto emptyB = std::make_shared<PagedEntryPayload>();
+  EXPECT_TRUE(StrictlyEqual(*emptyA, *emptyB));
 }
 
 TEST(EntryPayload, paged_payloads_are_not_strictly_equal_if_their_contents_differ) {
   const auto propsA = EntryProperties{.filesize = 1024, .pagesize = 512};
   const auto propsB = EntryProperties{.filesize = 2048, .pagesize = 512};
-  const auto pages_A = std::vector<PageId>{12};
-  const auto pages_B = std::vector<PageId>{35, 6};
+  const auto pagesA = std::vector<PageId>{12};
+  const auto pagesB = std::vector<PageId>{35, 6};
 
   auto props = AsPersistentEntryProperties(propsA);
-  const auto paged_AA = MakeEntryPayload<PagedEntryPayload>(props, pages_A); // constructor consumes props
+  const auto paged_AA = MakeEntryPayload<PagedEntryPayload>(props, pagesA); // constructor consumes props
 
   props = AsPersistentEntryProperties(propsA);
-  const auto paged_AB = MakeEntryPayload<PagedEntryPayload>(props, pages_B);
+  const auto paged_AB = MakeEntryPayload<PagedEntryPayload>(props, pagesB);
 
   props = AsPersistentEntryProperties(propsB);
-  const auto paged_BA = MakeEntryPayload<PagedEntryPayload>(props, pages_A);
+  const auto paged_BA = MakeEntryPayload<PagedEntryPayload>(props, pagesA);
 
   props = AsPersistentEntryProperties(propsB);
-  const auto paged_BB = MakeEntryPayload<PagedEntryPayload>(props, pages_B);
+  const auto paged_BB = MakeEntryPayload<PagedEntryPayload>(props, pagesB);
 
   EXPECT_FALSE(StrictlyEqual(*paged_AA, *paged_AB));
   EXPECT_FALSE(StrictlyEqual(*paged_AA, *paged_BA));
