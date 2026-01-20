2569b2c8fe6b38f909fe13db3a43e2cf283a044d
Steven WdV <swdv@cs.ru.nl>
2026-01-20 16:22:56 +0100

    WASM: Fix CurrentThreadPrintable on Node.js with sometimes non-string `name`

--- cpp/pep/weblib/ThreadPrintable.cpp
+++ cpp/pep/weblib/ThreadPrintable.cpp
@@ -5,16 +5,25 @@
 
 std::ostream& pep::weblib::operator<<(std::ostream& os, const CurrentThreadPrintable&) {
   const auto flags = os.flags();
-  os << std::hex << "0x" << ::pthread_self()
-      << ' ' << (::emscripten_is_main_runtime_thread() ? "main" : emscripten::val::global("name").as<std::string>());
+  os << std::hex << "0x" << ::pthread_self();
+  if (::emscripten_is_main_runtime_thread()) {
+    os << " main";
+  } else {
+    emscripten::val name = emscripten::val::global("name");
+    if (name.isString()) {
+      os << ' ' << name.as<std::string>();
+    }
+  }
   os.flags(flags);
   return os;
 }
 
 std::ostream& pep::weblib::operator<<(std::ostream& os, const ThreadPrintable& self) {
   const auto flags = os.flags();
-  os << std::hex << "0x" << self.thread_
-      << (self.thread_ == ::emscripten_main_runtime_thread_id() ? " (main)" : "");
+  os << std::hex << "0x" << self.thread_;
+  if (self.thread_ == ::emscripten_main_runtime_thread_id()) {
+    os << " (main)";
+  }
   os.flags(flags);
   return os;
 }
