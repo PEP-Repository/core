baea07c2f0dd95dc4519e20471f057c66d539e4e
Kai van Lopik <K.vanLopik@cs.ru.nl>
2026-02-24 17:09:56 +0100

    CSV export configuration now supports a "force_header" setting. Used this setting to fix failing unit tests.

--- cpp/pep/structuredoutput/Csv.cpp
+++ cpp/pep/structuredoutput/Csv.cpp
@@ -38,7 +38,7 @@ std::ostream& append(std::ostream& stream, Table::ConstRecordRef record, Config 
 
 std::ostream& append(std::ostream& stream, const Table& table, Config config) {
   auto records = table.records();
-  if (!records.empty()) {
+  if (config.force_header || !records.empty()) {
     append(stream, table.header(), config);
     for (const auto& record : records) {
       append(stream, record, config);
