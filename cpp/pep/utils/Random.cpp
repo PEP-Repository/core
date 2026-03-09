ecf4894ba6723a526e87d884baa85b6cc96ae82a
Steven WdV <swdv@cs.ru.nl>
2026-03-09 17:00:29 +0100

    Fix alignment issues in RandomBytes

--- cpp/pep/utils/Random.cpp
+++ cpp/pep/utils/Random.cpp
@@ -5,6 +5,7 @@
 #include <openssl/rand.h>
 
 #include <cassert>
+#include <memory>
 #include <random>
 
 void pep::UnbufferedRandomBytes(std::span<std::byte> out) {
@@ -16,28 +17,46 @@ void pep::UnbufferedRandomBytes(std::span<std::byte> out) {
 
 static_assert(std::uniform_random_bit_generator<pep::SecureUrbg>);
 
+static void AssignLeftoverRandomBytes(std::span<std::byte> outLeftover) {
+  if (!outLeftover.empty()) {
+    assert(outLeftover.size() < sizeof(typename pep::SecureUrbg::result_type));
+    auto finalNum = static_cast<std::make_unsigned_t<pep::SecureUrbg::result_type>>(pep::ThreadUrbg());
+    for (auto& to : outLeftover) {
+      to = static_cast<std::byte>(finalNum & 0xff);
+      finalNum >>= 8;
+    }
+  }
+}
+
 void pep::RandomBytes(std::span<std::byte> out) {
+  // Confirm that the URBG generates the full range
   static_assert(
     SecureUrbg::min() == std::numeric_limits<SecureUrbg::result_type>::min()
     && SecureUrbg::max() == std::numeric_limits<SecureUrbg::result_type>::max());
 
-  std::span outLarge(
-    reinterpret_cast<SecureUrbg::result_type*>(out.data()),
-    out.size() / sizeof(typename SecureUrbg::result_type));
-  assert(outLarge.size_bytes() <= out.size());
+  // First try to assign values as returned from the URBG.
+  // We align the pointer if necessary for a multi-byte data type.
+  void* alignPtr = out.data();
+  std::size_t spaceExclAlign = out.size();
+  static_assert(alignof(SecureUrbg::result_type) <= sizeof(SecureUrbg::result_type));
+  if constexpr (alignof(SecureUrbg::result_type) > 1) {
+    (void) std::align(alignof(SecureUrbg::result_type), 0, alignPtr, spaceExclAlign);
+  }
+  std::span outAligned(
+    static_cast<SecureUrbg::result_type*>(alignPtr),
+    spaceExclAlign / sizeof(typename SecureUrbg::result_type));
+  assert(outAligned.size_bytes() <= out.size());
   //XXX Replace by std::generate_random in C++26
-  std::ranges::generate(outLarge, std::ref(ThreadUrbg));
+  std::ranges::generate(outAligned, std::ref(ThreadUrbg));
 
-  if constexpr (sizeof(typename SecureUrbg::result_type) > 1) {
-    auto outLeftover = out.subspan(outLarge.size_bytes());
-    if (!outLeftover.empty()) {
-      assert(outLeftover.size() < sizeof(typename SecureUrbg::result_type));
-      auto finalNum = static_cast<std::make_unsigned_t<SecureUrbg::result_type>>(ThreadUrbg());
-      for (auto& to : outLeftover) {
-        to = static_cast<std::byte>(finalNum & 0xff);
-        finalNum >>= 8;
-      }
-    }
+  [[maybe_unused]] auto outAlignedBytes = std::as_writable_bytes(outAligned);
+  if constexpr (alignof(SecureUrbg::result_type) > 1) {
+    // Assign start
+    AssignLeftoverRandomBytes(std::span(out.data(), outAlignedBytes.data()));
+  }
+  if constexpr (sizeof(SecureUrbg::result_type) > 1) {
+    // Assign end
+    AssignLeftoverRandomBytes(std::span(outAlignedBytes.data() + outAlignedBytes.size(), out.data() + out.size()));
   }
 }
 
--- cpp/pep/utils/tests/Random.test.cpp
+++ cpp/pep/utils/tests/Random.test.cpp
@@ -15,19 +15,62 @@ TEST(RandomBytes, zeros) {
     << "Found suspiciously many zeros in random buffer of " << buf.size() << " bytes";
 }
 
-TEST(RandomBytes, tailZeros) {
+TEST(RandomBytes, smallZeros) {
   std::array<bool, sizeof(std::uint64_t) - 1> nonzeroSeen{};
   for (unsigned iteration{}; iteration < 150; ++iteration) {
     std::array<std::byte, nonzeroSeen.size()> buf{};
     pep::RandomBytes(buf);
-    for (std::size_t i{}; i < buf.size(); ++i) {
+    for (std::size_t i{}; i < nonzeroSeen.size(); ++i) {
       nonzeroSeen[i] |= buf[i] != std::byte{};
     }
   }
 
   std::array<bool, nonzeroSeen.size()> allTrue{};
   std::ranges::fill(allTrue, true);
-  EXPECT_EQ(nonzeroSeen, allTrue) << "Some bytes of tail in random buffer are always zero";
+  EXPECT_EQ(nonzeroSeen, allTrue) << "Some bytes in small random buffer are always zero";
+}
+
+TEST(RandomBytes, endZeros) {
+  std::array<bool, sizeof(std::uint64_t) - 1> nonzeroSeen{};
+  for (unsigned iteration{}; iteration < 150; ++iteration) {
+    std::array<std::byte, nonzeroSeen.size() + 2 * sizeof(std::uint64_t)> buf{};
+    pep::RandomBytes(buf);
+    for (std::size_t i{}; i < nonzeroSeen.size(); ++i) {
+      nonzeroSeen[i] |= buf[i + (buf.size() - nonzeroSeen.size())] != std::byte{};
+    }
+  }
+
+  std::array<bool, nonzeroSeen.size()> allTrue{};
+  std::ranges::fill(allTrue, true);
+  EXPECT_EQ(nonzeroSeen, allTrue) << "Some bytes at end of random buffer are always zero";
+}
+
+TEST(RandomBytes, unalignedEndZeros) {
+  std::array<bool, sizeof(std::uint64_t) - 1> nonzeroSeen{};
+  for (unsigned iteration{}; iteration < 150; ++iteration) {
+    alignas(8) std::array<std::byte, 1 + 2 * sizeof(std::uint64_t)> aligned{};
+    auto unaligned = std::span(aligned).subspan(1);
+    pep::RandomBytes(unaligned);
+    for (std::size_t i{}; i < nonzeroSeen.size(); ++i) {
+      nonzeroSeen[i] |= unaligned[i + (unaligned.size() - nonzeroSeen.size())] != std::byte{};
+    }
+  }
+
+  std::array<bool, nonzeroSeen.size()> allTrue{};
+  std::ranges::fill(allTrue, true);
+  EXPECT_EQ(nonzeroSeen, allTrue) << "Some bytes at end of un unaligned random buffer are always zero";
+}
+
+TEST(RandomBytes, unalignedStartZero) {
+  bool nonzeroSeen{};
+  for (unsigned iteration{}; iteration < 150; ++iteration) {
+    alignas(8) std::array<std::byte, 1 + 2 * sizeof(std::uint64_t)> aligned{};
+    auto unaligned = std::span(aligned).subspan(1);
+    pep::RandomBytes(unaligned);
+    nonzeroSeen |= unaligned.front() != std::byte{};
+  }
+
+  EXPECT_TRUE(nonzeroSeen) << "The byte at the start of an unaligned random buffer is always zero";
 }
 
 }
