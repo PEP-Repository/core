1ff8c3e3603e85543d0cad4b6cc4738c45a6d4e9
Steven WdV <swdv@cs.ru.nl>
2026-03-10 13:12:53 +0100

    Improve RandomBytes tests

--- cpp/pep/utils/tests/Random.test.cpp
+++ cpp/pep/utils/tests/Random.test.cpp
@@ -18,7 +18,8 @@ TEST(RandomBytes, zeros) {
 TEST(RandomBytes, smallZeros) {
   std::array<bool, sizeof(std::uint64_t) - 1> nonzeroSeen{};
   for (unsigned iteration{}; iteration < 150; ++iteration) {
-    std::array<std::byte, nonzeroSeen.size()> buf{};
+    // Layout in aligned blocks: BBBBBBB_
+    alignas(std::uint64_t) std::array<std::byte, nonzeroSeen.size()> buf{};
     pep::RandomBytes(buf);
     for (std::size_t i{}; i < nonzeroSeen.size(); ++i) {
       nonzeroSeen[i] |= buf[i] != std::byte{};
@@ -27,13 +28,33 @@ TEST(RandomBytes, smallZeros) {
 
   std::array<bool, nonzeroSeen.size()> allTrue{};
   std::ranges::fill(allTrue, true);
-  EXPECT_EQ(nonzeroSeen, allTrue) << "Some bytes in small random buffer are always zero";
+  EXPECT_EQ(nonzeroSeen, allTrue) << "Some bytes in a small random buffer are always zero";
+}
+
+TEST(RandomBytes, smallUnalignedZeros) {
+  std::array<bool, sizeof(std::uint64_t) * 2 - 2> nonzeroSeen{};
+  for (unsigned iteration{}; iteration < 150; ++iteration) {
+    alignas(sizeof(std::uint64_t)) std::array<std::byte, 1 + nonzeroSeen.size()> aligned{};
+    // This is larger than a uint64, but because of alignment,
+    // RandomBytes may only be allowed to write bytes.
+    // Layout in aligned blocks: _BBBBBBB BBBBBBB_
+    auto unaligned = std::span(aligned).subspan(1);
+    pep::RandomBytes(unaligned);
+    for (std::size_t i{}; i < nonzeroSeen.size(); ++i) {
+      nonzeroSeen[i] |= unaligned[i] != std::byte{};
+    }
+  }
+
+  std::array<bool, nonzeroSeen.size()> allTrue{};
+  std::ranges::fill(allTrue, true);
+  EXPECT_EQ(nonzeroSeen, allTrue) << "Some bytes in a small unaligned random buffer are always zero";
 }
 
 TEST(RandomBytes, endZeros) {
   std::array<bool, sizeof(std::uint64_t) - 1> nonzeroSeen{};
   for (unsigned iteration{}; iteration < 150; ++iteration) {
-    std::array<std::byte, nonzeroSeen.size() + 2 * sizeof(std::uint64_t)> buf{};
+    // Layout in aligned blocks: BBBBBBBB BBBBBBBB BBBBBBB_
+    alignas(std::uint64_t) std::array<std::byte, nonzeroSeen.size() + 2 * sizeof(std::uint64_t)> buf{};
     pep::RandomBytes(buf);
     for (std::size_t i{}; i < nonzeroSeen.size(); ++i) {
       nonzeroSeen[i] |= buf[i + (buf.size() - nonzeroSeen.size())] != std::byte{};
@@ -42,13 +63,14 @@ TEST(RandomBytes, endZeros) {
 
   std::array<bool, nonzeroSeen.size()> allTrue{};
   std::ranges::fill(allTrue, true);
-  EXPECT_EQ(nonzeroSeen, allTrue) << "Some bytes at end of random buffer are always zero";
+  EXPECT_EQ(nonzeroSeen, allTrue) << "Some bytes at the end of a random buffer are always zero";
 }
 
 TEST(RandomBytes, unalignedEndZeros) {
   std::array<bool, sizeof(std::uint64_t) - 1> nonzeroSeen{};
   for (unsigned iteration{}; iteration < 150; ++iteration) {
-    alignas(8) std::array<std::byte, 1 + 2 * sizeof(std::uint64_t)> aligned{};
+    alignas(sizeof(std::uint64_t)) std::array<std::byte, 1 + 2 * sizeof(std::uint64_t)> aligned{};
+    // Layout in aligned blocks: _BBBBBBB BBBBBBBB B_______
     auto unaligned = std::span(aligned).subspan(1);
     pep::RandomBytes(unaligned);
     for (std::size_t i{}; i < nonzeroSeen.size(); ++i) {
@@ -58,19 +80,24 @@ TEST(RandomBytes, unalignedEndZeros) {
 
   std::array<bool, nonzeroSeen.size()> allTrue{};
   std::ranges::fill(allTrue, true);
-  EXPECT_EQ(nonzeroSeen, allTrue) << "Some bytes at end of un unaligned random buffer are always zero";
+  EXPECT_EQ(nonzeroSeen, allTrue) << "Some bytes at the end of an unaligned random buffer are always zero";
 }
 
-TEST(RandomBytes, unalignedStartZero) {
-  bool nonzeroSeen{};
+TEST(RandomBytes, unalignedStartZeros) {
+  std::array<bool, sizeof(std::uint64_t) - 1> nonzeroSeen{};
   for (unsigned iteration{}; iteration < 150; ++iteration) {
-    alignas(8) std::array<std::byte, 1 + 2 * sizeof(std::uint64_t)> aligned{};
+    alignas(sizeof(std::uint64_t)) std::array<std::byte, 1 + 2 * sizeof(std::uint64_t)> aligned{};
+    // Layout in aligned blocks: _BBBBBBB BBBBBBBB B_______
     auto unaligned = std::span(aligned).subspan(1);
     pep::RandomBytes(unaligned);
-    nonzeroSeen |= unaligned.front() != std::byte{};
+    for (std::size_t i{}; i < nonzeroSeen.size(); ++i) {
+      nonzeroSeen[i] |= unaligned[i] != std::byte{};
+    }
   }
 
-  EXPECT_TRUE(nonzeroSeen) << "The byte at the start of an unaligned random buffer is always zero";
+  std::array<bool, nonzeroSeen.size()> allTrue{};
+  std::ranges::fill(allTrue, true);
+  EXPECT_EQ(nonzeroSeen, allTrue) << "Some bytes at the start of an unaligned random buffer are always zero";
 }
 
 }
