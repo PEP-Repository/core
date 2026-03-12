b8aa666e34a94f69725028cd2a83ecb087382290
Linus van Elswijk <l.vanelswijk@science.ru.nl>
2026-03-12 14:53:49 +0100

    refac(declpep): introduce `jq` wrapper function
    Make the code easier to read by using a custom function to call jq:
      - Makes passing `-r` and `$json_file` implicit
      - Allows us to more easily split the format string over multiple lines
      - Takes care of enclosing the format string with double quotes before
        passing it to `jq`. This makes escaping double quotes (") more
    straight,
        because we stripped one layer of evaluation/expansion.

--- tests/declpep.sh
+++ tests/declpep.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2016 # Reason: we are intentionally using single quoted strings to avoid expansion
 
 json_file="$1"
 if [ ! -f "$json_file" ]; then
@@ -6,52 +7,61 @@ if [ ! -f "$json_file" ]; then
   exit 1
 fi
 
+# Runs a jq filter on $json_file and then feeds this into a jq format string
+# The first argument specifies the filter
+# The other arguments are concatenated into a single format string
+jqr() {
+  # Removes leading and trailing whitespace from each line of input.
+  trim() {
+    sed 's/^[[:space:]]*//;s/[[:space:]]*$//'
+  }
+
+  local filter=$1
+  shift
+  local genlines=${*//\"/\\\"}
+
+  jq -r "$filter | \"$genlines\"" "$json_file" | trim
+}
+
 # user groups
-jq -r '.userGroups[] |
-  "pepcli --oauth-token-group \"Access Administrator\" user group create \(.)"' \
-  "$json_file"
+jqr '.userGroups[]' \
+  'pepcli --oauth-token-group "Access Administrator" user group create \(.)'
 
 echo ""
 
 # column groups
-jq -r '.columnGroups[] |
-  "pepcli --oauth-token-group \"Data Administrator\" ama columnGroup create \(.name)"' \
-  "$json_file"
+jqr '.columnGroups[]' \
+  'pepcli --oauth-token-group "Data Administrator" ama columnGroup create \(.name)'
 
 # column group access rules
-jq -r '.columnGroups[] | .name as $group | .cgars[] |
-  "pepcli --oauth-token-group \"Access Administrator\" ama cgar create \($group) \(.userGroup) \(.permissions[])"' \
-  "$json_file"
+jqr '.columnGroups[] | .name as $group | .cgars[]' \
+  'pepcli --oauth-token-group "Access Administrator" ama cgar create \($group) \(.userGroup) \(.permissions[])'
 
 echo ""
 
 # individual columns
-jq -r '.columnGroups[] | .name as $group | .columns[] |
-  "pepcli --oauth-token-group \"Data Administrator\" ama column create \(.)\npepcli --oauth-token-group \"Data Administrator\" ama column addTo \(.) \($group)"' \
-  "$json_file"
+jqr '.columnGroups[] | .name as $group | .columns[]' \
+  'pepcli --oauth-token-group "Data Administrator" ama column create \(.)' "\n" \
+  'pepcli --oauth-token-group "Data Administrator" ama column addTo \(.) \($group)'
 
 echo ""
 
 # subject groups
-jq -r '.participantGroups[] |
-  "pepcli --oauth-token-group \"Data Administrator\" ama group create \(.name)"' \
-  "$json_file"
+jqr '.participantGroups[]' \
+  'pepcli --oauth-token-group "Data Administrator" ama group create \(.name)'
 
 # subject group access rules
-jq -r '.participantGroups[].pgars[] |
-  "pepcli --oauth-token-group \"Access Administrator\" ama pgar create \(.userGroup) \(.permissions[])"' \
-  "$json_file"
+jqr '.participantGroups[].pgars[]' \
+  'pepcli --oauth-token-group "Access Administrator" ama pgar create \(.userGroup) \(.permissions[])'
 
 echo ""
 
 # individual subjects
-jq -r '.participantGroups[] | .name as $group | .participants | to_entries[] | "ID_\($group)_\(.key)" as $bash_var |
-  "\($bash_var)=\"$(pepcli --oauth-token-group \"Data Administrator\" register id)\"\npepcli --oauth-token-group \"Data Administrator\" ama group addTo \($group) \"${\($bash_var)}\""' \
-  "$json_file"
+jqr '.participantGroups[] | .name as $group | .participants | to_entries[] | "ID_\($group)_\(.key)" as $bash_var' \
+  '\($bash_var)="$(pepcli --oauth-token-group "Data Administrator" register id)"' "\n" \
+  'pepcli --oauth-token-group "Data Administrator" ama group addTo \($group) "${\($bash_var)}"'
 
 echo ""
 
-jq -r '.participantGroups[] | .name as $group | .participants | to_entries[] | "ID_\($group)_\(.key)" as $bash_var | .value | to_entries[] |
-  "pepcli --oauth-token-group \"Data Administrator\" store -p \"${\($bash_var)}\" -c \"\(.key)\" -d \"\(.value)\""' \
-  "$json_file"
-
+jqr '.participantGroups[] | .name as $group | .participants | to_entries[] | "ID_\($group)_\(.key)" as $bash_var | .value | to_entries[]' \
+  'pepcli --oauth-token-group "Data Administrator" store -p "${\($bash_var)}" -c "\(.key)" -d "\(.value)"'
