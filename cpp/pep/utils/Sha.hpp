6393fdb5a9b5117bf45876d5247a57279d49b827
Steven WdV <swdv@cs.ru.nl>
2026-03-16 12:44:59 +0100

    Check errors in SHA hasher & move hasher implementations to cpp

--- cpp/pep/utils/Sha.hpp
+++ cpp/pep/utils/Sha.hpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <array>
 #include <cstdint>
-#include <ranges>
+#include <span>
 #include <string>
 
 #include <openssl/evp.h>
@@ -15,53 +15,31 @@ namespace pep {
 
 namespace detail {
 
-template <class TDerived, size_t OUTPUTSIZE, size_t BLOCKSIZE>
-class Sha : public Hasher<std::string> {
-public:
-  using typename Hasher<std::string>::Hash;
-
-private:
+class ShaBase : public Hasher<std::string> {
   EVP_MD_CTX* mdctx = nullptr;
 
 protected:
-  Sha() = default;
-
-  Hash finish() override {
-    std::string ret(OUTPUTSIZE, '\0');
-    digestInto(ConvertBytes<std::uint8_t>(std::span<char, OUTPUTSIZE>(ret)));
-    return ret;
-  }
-
   virtual const EVP_MD* getDigestType() const = 0;
 
-private:
   // We can't initialize in the constructor because we use the virtual method "getDigestType"
-  void ensureInitialized() {
-    if (mdctx == nullptr) {
-      mdctx = EVP_MD_CTX_new();
-      EVP_DigestInit_ex(mdctx, this->getDigestType(), nullptr);
-    }
-  }
+  EVP_MD_CTX* ensureInitialized();
 
-  void digestInto(std::span<std::uint8_t, OUTPUTSIZE> out) {
-    this->ensureInitialized();
-    unsigned int s{};
-    EVP_DigestFinal_ex(mdctx, out.data(), &s);
-    assert(s == out.size());
-  }
+  void digestInto(std::span<std::uint8_t> out);
 
-protected:
-  void process(const void* block, size_t size) override {
-    this->ensureInitialized();
-    EVP_DigestUpdate(mdctx, block, size);
-  }
+  void process(const void* block, size_t size) override;
+
+  Hash finishImpl(size_t outputSize);
 
 public:
-  ~Sha() noexcept override {
-    if (mdctx != nullptr) {
-      EVP_MD_CTX_free(mdctx);
-    }
-  }
+  ~ShaBase() noexcept override;
+};
+
+template <class TDerived, size_t OUTPUTSIZE, size_t BLOCKSIZE>
+class Sha : public ShaBase {
+protected:
+  Sha() = default;
+
+  Hash finish() override { return finishImpl(OUTPUTSIZE); }
 
 public:
   static std::string HMac(std::string_view key, std::string_view data);
@@ -115,12 +93,12 @@ std::string Sha<TDerived, OUTPUTSIZE, BLOCKSIZE>::HMac(std::string_view key, std
 
 class Sha256 : public detail::Sha<Sha256, 32, 64> {
 protected:
-  const EVP_MD* getDigestType() const override { return EVP_sha256(); }
+  const EVP_MD* getDigestType() const override;
 };
 
 class Sha512 : public detail::Sha<Sha512, 64, 128> {
 protected:
-  const EVP_MD* getDigestType() const override { return EVP_sha512(); }
+  const EVP_MD* getDigestType() const override;
 };
 
 }
