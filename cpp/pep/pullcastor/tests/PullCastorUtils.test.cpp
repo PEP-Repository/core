ae18caea4862f2e1565da6e8fff0022a92d94daa
Steven WdV <swdv@cs.ru.nl>
2026-03-19 16:57:00 +0100

    Add tests for `castor::ParseCastorDateTime` Closes #2840

---
+++ cpp/pep/pullcastor/tests/PullCastorUtils.test.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+
+#include <pep/pullcastor/PullCastorUtils.hpp>
+
+#include <boost/property_tree/ptree.hpp>
+
+using namespace std::literals;
+using namespace pep;
+
+namespace {
+
+TEST(PullCastorUtils, ParseCastorDateTime_Amsterdam) {
+  boost::property_tree::ptree datetimeObject;
+  datetimeObject.put("date", "2020-11-19 13:49:44.000000");
+  datetimeObject.put("timezone", "Europe/Amsterdam");
+  EXPECT_EQ(castor::ParseCastorDateTime(datetimeObject), Timestamp{1605790184s});
+}
+
+TEST(PullCastorUtils, ParseCastorDateTime_UTC) {
+  boost::property_tree::ptree datetimeObject;
+  datetimeObject.put("date", "2020-11-19 13:49:44.000000");
+  datetimeObject.put("timezone", "Etc/UTC");
+  EXPECT_EQ(castor::ParseCastorDateTime(datetimeObject), Timestamp{1605793784s});
+}
+
+}
