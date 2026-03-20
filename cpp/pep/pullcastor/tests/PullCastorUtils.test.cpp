fada2f44ebff53bb294379d9d07efe401450c96f
Steven WdV <swdv@cs.ru.nl>
2026-03-20 10:17:31 +0100

    Add CEST test to PullCastorUtils

--- cpp/pep/pullcastor/tests/PullCastorUtils.test.cpp
+++ cpp/pep/pullcastor/tests/PullCastorUtils.test.cpp
@@ -9,21 +9,28 @@ using namespace pep;
 
 namespace {
 
-TEST(PullCastorUtils, ParseCastorDateTime_Amsterdam) {
+TEST(ParseCastorDateTime, Amsterdam) {
   boost::property_tree::ptree datetimeObject;
   datetimeObject.put("date", "2020-11-19 13:49:44.000000");
   datetimeObject.put("timezone", "Europe/Amsterdam");
   EXPECT_EQ(castor::ParseCastorDateTime(datetimeObject), Timestamp{1605790184s});
 }
 
-TEST(PullCastorUtils, ParseCastorDateTime_UTC) {
+TEST(ParseCastorDateTime, AmsterdamSummer) {
+  boost::property_tree::ptree datetimeObject;
+  datetimeObject.put("date", "2020-06-19 13:49:44.000000");
+  datetimeObject.put("timezone", "Europe/Amsterdam");
+  EXPECT_EQ(castor::ParseCastorDateTime(datetimeObject), Timestamp{1592567384s});
+}
+
+TEST(ParseCastorDateTime, Utc) {
   boost::property_tree::ptree datetimeObject;
   datetimeObject.put("date", "2020-11-19 13:49:44.000000");
   datetimeObject.put("timezone", "Etc/UTC");
   EXPECT_EQ(castor::ParseCastorDateTime(datetimeObject), Timestamp{1605793784s});
 }
 
-TEST(PullCastorUtils, ParseCastorDateTime_Plus5) {
+TEST(ParseCastorDateTime, UtcPlus5) {
   boost::property_tree::ptree datetimeObject;
   datetimeObject.put("date", "2020-11-19 13:49:44.000000");
   datetimeObject.put("timezone", "Indian/Maldives"); // UTC+5 a.k.a. Etc/GMT-5
