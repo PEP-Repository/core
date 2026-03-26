a0c4f52921310a531616c7fe7e9a8444531708c8
Kai van Lopik <K.vanLopik@cs.ru.nl>
2026-03-26 09:38:01 +0100

    Getting rid of conversion warnings: let's see if clang finds this more palatable.

--- cpp/pep/messaging/tests/MessageSequence.test.cpp
+++ cpp/pep/messaging/tests/MessageSequence.test.cpp
@@ -7,7 +7,21 @@
 #include <fstream>
 
 TEST(MessageSequence, IStreamIsBatchedLazily) {
-  auto bytes = static_cast<size_t>(pep::messaging::DEFAULT_PAGE_SIZE * 4.5);
+  /* So I want the stream to produce a couple of pages, ending with a page that isn't entirely full.
+   * The naive and readable implementation would be
+   *    const size_t bytes = pep::messaging::DEFAULT_PAGE_SIZE * 4.5;
+   * but we'd have to static_cast the multiplication result to prevent conversion warnings:
+   *    const auto bytes = static_cast<size_t>(pep::messaging::DEFAULT_PAGE_SIZE * 4.5);
+   * Then Clang produces another conversion warning about the constant being promoted to double when it serves as
+   * an operand to the multiplication. So we'd have to add another static_cast, yielding
+   *    const auto bytes = static_cast<size_t>(static_cast<double>(pep::messaging::DEFAULT_PAGE_SIZE) * 4.5);
+   * IMO that would be just as stupid and unreadable as this non-converting implementation:
+   */
+  const auto bytes
+    = pep::messaging::DEFAULT_PAGE_SIZE * 4U  // Please give me four full pages...
+    + pep::messaging::DEFAULT_PAGE_SIZE / 2U; // ... and another half-full page.
+  // </rant>
+
   auto file = pep::filesystem::Temporary::MakeFile(pep::RandomString(bytes));
   auto stream = std::make_shared<std::ifstream>(file.path().c_str(), std::ios_base::in | std::ios_base::binary);
 
