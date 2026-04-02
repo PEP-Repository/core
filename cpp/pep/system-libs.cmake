b10726ea606162483b9a0f99d0737b9b325f2d88
Steven WdV <swdv@cs.ru.nl>
2026-04-02 11:22:07 +0200

    Use FindThreads instead of `-pthread` flag

--- cpp/pep/system-libs.cmake
+++ cpp/pep/system-libs.cmake
@@ -4,13 +4,6 @@
 # their PARENT_SCOPE, those variables will be available where they're needed
 # and not just in this file.
 
-# ---------- Threads ----------
-
-find_package(Threads)
-if (CMAKE_SYSTEM_NAME MATCHES "Linux|Emscripten")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread")
-endif()
-
 # ---------- Boost ----------
 
 if(NOT ${CMAKE_SYSTEM_NAME} MATCHES "Windows")
