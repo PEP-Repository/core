52ae132e9d81a7f5a62af039bb4f8468114ec181
Linus van Elswijk <l.vanelswijk@science.ru.nl>
2026-04-28 16:07:55 +0200

    test(EnumUtils): Add EnumUtils.cpp with static asserts
    Make the compiler guard against some errors

---
+++ cpp/pep/utils/EnumUtils.cpp
@@ -0,0 +1,39 @@
+#include <pep/utils/EnumUtils.hpp>
+
+namespace {
+
+enum class TestedFlags {
+  None = 0,
+  First = 0b01,
+  Second = 0b10,
+  All = 0b11,
+};
+
+}
+
+PEP_MARK_AS_FLAG_ENUM_TYPE(TestedFlags);
+
+namespace pep {
+
+// the binary operators are trivial, because they just wrap casts to/from the underlying type
+// `operator~` is non-trivial: the implementation needs to mask out bits that would be out-of-range
+static_assert(~TestedFlags::None == TestedFlags::All, "operator~ follows bitwise inversion");
+static_assert(~TestedFlags::First == TestedFlags::Second, "operator~ follows bitwise inversion");
+static_assert(~TestedFlags::Second == TestedFlags::First, "operator~ follows bitwise inversion");
+static_assert(~TestedFlags::All == TestedFlags::None, "operator~ follows bitwise inversion");
+
+static_assert(TestFlags(TestedFlags::All, TestedFlags::Second),
+    "TestFlags(x, y) tests if all flags that are set in y are also set in x");
+static_assert(!TestFlags(TestedFlags::First, TestedFlags::Second),
+    "TestFlags(x, y) tests if all flags that are set in y are also set in x");
+static_assert(TestFlags(TestedFlags::None, TestedFlags::None),
+    "Edge Case: TestFlags(t, T::None) is always true");
+static_assert(TestFlags(TestedFlags::All, TestedFlags::None),
+    "Edge Case: TestFlags(t, T::None) is always true");
+
+static_assert(ConditionalFlags(TestedFlags::Second, true) == TestedFlags::Second,
+    "ConditionalFlags(x, true) returns x");
+static_assert(ConditionalFlags(TestedFlags::Second, false) == TestedFlags::None,
+    "ConditionalFlags(x, false) returns None");
+
+}
