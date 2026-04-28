59b6006916031ba2ee4b61aea18d44625c4ce67d
Steven WdV <swdv@cs.ru.nl>
2026-04-28 17:33:44 +0200

    WASM: Move code providing peak memory usage from TestApplicationMainFunction.cpp to ApplicationMetrics

--- cpp/pep/utils/TestApplicationMainFunction.cpp
+++ cpp/pep/utils/TestApplicationMainFunction.cpp
@@ -1,12 +1,8 @@
+#include <pep/utils/ApplicationMetrics.hpp>
 #include <pep/utils/RegisteredTestEnvironment.hpp>
 #include <pep/utils/Log.hpp>
 
-#ifndef _WIN32
-#include <sys/resource.h>
-#else
-#include <Windows.h>
-#include <Psapi.h>
-#endif
+#include <cmath>
 
 int main(int argc, char* argv[]) {
   auto env = pep::RegisteredTestEnvironment::Create(argc, argv);
@@ -21,27 +17,10 @@ int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   int retval = RUN_ALL_TESTS();
 
-  // getrusage not supported on Emscripten
-#ifndef __EMSCRIPTEN__
-  uint64_t maxmem{};
-#ifndef _WIN32
-  rusage usage{};
-  ::getrusage(RUSAGE_SELF, &usage);
-  maxmem = static_cast<uint64_t>(usage.ru_maxrss);
-#if defined(__APPLE__) && defined(__MACH__)
-  maxmem /= 1024;
-#endif
-#else
-  PROCESS_MEMORY_COUNTERS pmc;
-  if (!::GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc))) {
-    std::cerr << "Could not read memory info" << std::endl;
-    exit(2);
+  if (const auto maxmem = pep::ApplicationMetrics::GetMemoryUsageBytes();
+      !std::isnan(maxmem)) {
+    std::cout << (maxmem / (1024 * 1024)) << " megabytes of memory used at max" << std::endl;
   }
-  maxmem = pmc.PeakWorkingSetSize / 1024;
-#endif
-  std::cout << maxmem << " kilobytes of memory used at max" << std::endl;
-  std::cout << (maxmem / 1024) << " megabytes of memory used at max" << std::endl;
-#endif
 
   return retval;
 }
