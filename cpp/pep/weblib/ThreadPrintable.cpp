1753b2fae6f9edf9d5f44e86a6b5ff7cca2f19ae
Steven WdV <swdv@cs.ru.nl>
2026-04-30 17:14:13 +0200

    WASM: Use PEP_DEFER to reset ostream flags in ThreadPrintable

--- cpp/pep/weblib/ThreadPrintable.cpp
+++ cpp/pep/weblib/ThreadPrintable.cpp
@@ -1,10 +1,13 @@
 #include <pep/weblib/ThreadPrintable.hpp>
 
+#include <pep/utils/Defer.hpp>
+
 #include <emscripten/threading.h>
 #include <emscripten/val.h>
 
 std::ostream& pep::weblib::operator<<(std::ostream& os, const CurrentThreadPrintable&) {
   const auto flags = os.flags();
+  PEP_DEFER(os.flags(flags));
   os << std::hex << "0x" << ::pthread_self();
   if (::emscripten_is_main_runtime_thread()) {
     os << " main";
@@ -14,16 +17,15 @@ std::ostream& pep::weblib::operator<<(std::ostream& os, const CurrentThreadPrint
       os << ' ' << name.as<std::string>();
     }
   }
-  os.flags(flags);
   return os;
 }
 
 std::ostream& pep::weblib::operator<<(std::ostream& os, const ThreadPrintable& self) {
   const auto flags = os.flags();
+  PEP_DEFER(os.flags(flags));
   os << std::hex << "0x" << self.thread_;
   if (self.thread_ == ::emscripten_main_runtime_thread_id()) {
     os << " (main)";
   }
-  os.flags(flags);
   return os;
 }
