643bbc14a9038d1234fcfc57b0dfcb7ece0899f0
Linus van Elswijk <l.vanelswijk@science.ru.nl>
2026-04-30 12:35:41 +0200

    chore(UserIdFlags): Prefer binary literals
    Review suggestion

--- cpp/pep/accessmanager/UserIdFlags.hpp
+++ cpp/pep/accessmanager/UserIdFlags.hpp
@@ -8,9 +8,9 @@ namespace pep {
 
 enum class PEP_ATTRIBUTE_FLAG_ENUM UserIdFlags : uint8_t {
   None = 0,
-  IsPrimaryId = 1 << 0,
-  IsDisplayId = 1 << 1,
-  All = (1 << 2) - 1,
+  IsPrimaryId = 0b01,
+  IsDisplayId = 0b10,
+  All = 0b11,
 };
 
 PEP_MARK_AS_FLAG_ENUM_TYPE(UserIdFlags)
