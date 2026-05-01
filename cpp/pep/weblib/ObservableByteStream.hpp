906e31091e446fa6e0a4cbc59f7b52660e634700
Steven WdV <swdv@cs.ru.nl>
2026-05-01 17:12:57 +0200

    WASM: Clarify CreateReadableByteStream chunkSize remark

--- cpp/pep/weblib/ObservableByteStream.hpp
+++ cpp/pep/weblib/ObservableByteStream.hpp
@@ -11,7 +11,10 @@ namespace pep::weblib {
 
 /// Create a ReadableStream that reuses buffers via \c autoAllocateChunkSize.
 /// Items will be emitted on the same thread as the observable.
-/// \param chunkSize The maximum size of each string in \p data. If this is too small, additional buffers will need to be allocated.
+/// \param chunkSize The maximum size of each string in \p data.
+/// \remark If \p chunkSize is too small for a string in \p data,
+///   the implementation may need to allocate extra ArrayBuffers, instead of being able to reuse the same one,
+///   which is less efficient.
 /// \returns \c ReadableStream<Uint8Array<ArrayBuffer>>
 emscripten::val CreateReadableByteStream(rxcpp::observable<std::string> data, std::size_t chunkSize);
 
