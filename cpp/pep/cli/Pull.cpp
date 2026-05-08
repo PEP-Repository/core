718ec3ff372dc476777388296e4288a0f350aeb5
Steven WdV <swdv@cs.ru.nl>
2026-05-08 17:01:22 +0200

    Fix download directory with trailing slash Closes #2196

--- cpp/pep/cli/Pull.cpp
+++ cpp/pep/cli/Pull.cpp
@@ -31,6 +31,7 @@ using namespace pep::enumUtils;
 using namespace pep::cli;
 using namespace std::chrono_literals;
 namespace so = pep::structuredOutput;
+namespace fs = std::filesystem;
 
 namespace {
 constexpr auto SUPPORTED_EXPORT_FORMATS = so::FormatFlags::Csv | so::FormatFlags::Json | so::FormatFlags::Yaml;
@@ -63,17 +64,17 @@ bool UsesSavedConfig(const std::shared_ptr<Context> &ctx) {
 * \param source Path to the original file or directory
 * \param dest Path to the directory in which to place the copies
 */
-void HardlinkFolders(std::filesystem::path source, std::filesystem::path dest) {
-  for (const auto& entry : std::filesystem::directory_iterator(source)) {
-    if (!std::filesystem::is_directory(entry)) {
+void HardlinkFolders(fs::path source, fs::path dest) {
+  for (const auto& entry : fs::directory_iterator(source)) {
+    if (!fs::is_directory(entry)) {
       auto filename = entry.path().filename();
-      std::filesystem::path newPath = dest / filename;
-      std::filesystem::create_hard_link(entry, newPath);
+      fs::path newPath = dest / filename;
+      fs::create_hard_link(entry, newPath);
     }
     else {
-      std::filesystem::path appendedSource = source / entry.path().filename();
-      std::filesystem::path appendedDest = dest / entry.path().filename();
-      std::filesystem::create_directory(appendedDest);
+      fs::path appendedSource = source / entry.path().filename();
+      fs::path appendedDest = dest / entry.path().filename();
+      fs::create_directory(appendedDest);
       HardlinkFolders(appendedSource, appendedDest);
     }
   }
@@ -84,20 +85,20 @@ void HardlinkFolders(std::filesystem::path source, std::filesystem::path dest) {
 * \param directory Path to the output directory
 * \param globalConfig Shared pointer to the GlobalConfiguration
 */
-void UpdateFormat(std::filesystem::path directory, std::shared_ptr<pep::GlobalConfiguration> globalConfig) {
+void UpdateFormat(fs::path directory, std::shared_ptr<pep::GlobalConfiguration> globalConfig) {
   auto metadataDir = directory / DownloadMetadata::GetDirectoryName();
-  for (const std::filesystem::directory_entry& entry : std::filesystem::directory_iterator(directory)) {
+  for (const fs::directory_entry& entry : fs::directory_iterator(directory)) {
     std::string entryName = entry.path().filename().string();
     auto metaPath = metadataDir / entryName;
-    if (std::filesystem::is_directory(entry)
-      && !std::filesystem::equivalent(entry.path(), metadataDir)
+    if (fs::is_directory(entry)
+      && !fs::equivalent(entry.path(), metadataDir)
       && !globalConfig->getUserPseudonymFormat().matches(entryName)
-      && std::filesystem::exists(metaPath)
+      && fs::exists(metaPath)
       && entryName.size() == pep::LocalPseudonym::TextLength()) {
       auto lp = pep::LocalPseudonym::FromText(entryName);
       std::string participantAlias = globalConfig->getUserPseudonymFormat().makeUserPseudonym(lp);
-      std::filesystem::rename(entry.path(), entry.path().parent_path() / participantAlias);
-      std::filesystem::rename(metaPath, metaPath.parent_path() / participantAlias);
+      fs::rename(entry.path(), entry.path().parent_path() / participantAlias);
+      fs::rename(metaPath, metaPath.parent_path() / participantAlias);
     }
   }
 }
@@ -124,13 +125,13 @@ void ReportProgress(const pep::Progress& progress) {
 */
 void prepareTempDirectory(const std::shared_ptr<Context> &ctx) {
   // Existing temp directory should be left intact only if --resume is specified
-  if (std::filesystem::exists(ctx->tempDirectory) && !ctx->resume) {
+  if (fs::exists(ctx->tempDirectory) && !ctx->resume) {
     // Updating but not resuming: we'll need a fresh temp directory
-    std::filesystem::remove_all(ctx->tempDirectory);
+    fs::remove_all(ctx->tempDirectory);
   }
 
-  if (!std::filesystem::exists(ctx->tempDirectory)) {
-    std::filesystem::create_directory(ctx->tempDirectory);
+  if (!fs::exists(ctx->tempDirectory)) {
+    fs::create_directory(ctx->tempDirectory);
     HardlinkFolders(ctx->outputDirectory, ctx->tempDirectory);
   }
 }
@@ -156,13 +157,13 @@ void checkContextSettings(const std::shared_ptr<Context> &ctx) {
   ctx->progress->advance("Checking local data");
   if (ctx->update || ctx->updateFormat) {
     // Existing temp directory should be left intact only if --resume is specified
-    if (std::filesystem::exists(ctx->tempDirectory) && !ctx->resume) {
+    if (fs::exists(ctx->tempDirectory) && !ctx->resume) {
       // User wants to update and we'll discard the temp directory: we'll need an output directory to work with
-      if (!std::filesystem::exists(ctx->outputDirectory)) {
+      if (!fs::exists(ctx->outputDirectory)) {
         throw std::runtime_error("Didn't find a source directory with name: " + ctx->outputDirectory + " to update");
       }
       // Verify that user really wants temp directory to be discarded
-      if (!ctx->force && !std::filesystem::is_empty(ctx->tempDirectory)) {
+      if (!ctx->force && !fs::is_empty(ctx->tempDirectory)) {
         throw std::runtime_error("Temporary download directory " + ctx->tempDirectory +
           " already exists. Specify --force to clear this directory or --resume to resume the download from this directory");
       }
@@ -173,10 +174,10 @@ void checkContextSettings(const std::shared_ptr<Context> &ctx) {
   }
   else {
     if (!ctx->force) {
-      if (std::filesystem::exists(ctx->outputDirectory) && !std::filesystem::is_empty(ctx->outputDirectory)) {
+      if (fs::exists(ctx->outputDirectory) && !fs::is_empty(ctx->outputDirectory)) {
         throw std::runtime_error("Output directory " + ctx->outputDirectory + " already exists. Specify --force to clear the directory and download anyway");
       }
-      if (std::filesystem::exists(ctx->tempDirectory) && !std::filesystem::is_empty(ctx->tempDirectory)) {
+      if (fs::exists(ctx->tempDirectory) && !fs::is_empty(ctx->tempDirectory)) {
         throw std::runtime_error("Temporary download directory " + ctx->tempDirectory + " already exists. Specify --force to clear the directory and download anyway");
       }
     }
@@ -204,6 +205,24 @@ so::FormatFlags ParseExportFormats(const std::vector<std::string>& formatNames) 
   return flags;
 }
 
+fs::path AppendDirectoryNameSuffix(const fs::path& path, std::string_view suffix) {
+  if (path.empty()) {
+    throw std::runtime_error("Directory path is empty");
+  }
+  // Collapse "." and ".."
+  auto normalPath = path.lexically_normal();
+  if (!normalPath.has_filename()) {
+    // Remove trailing separator
+    // "/path/to/dir/" -> "/path/to/dir"
+    normalPath = normalPath.parent_path();
+  }
+  if (!normalPath.has_filename()) { // E.g. "/"
+    throw std::runtime_error("Directory path is filesystem root: " + path.string());
+  }
+  normalPath.replace_filename(normalPath.filename() += suffix);
+  return normalPath;
+}
+
 /*!
 * \brief Based on the values given by the user, create a Context object that contains all required data to perform the download.
 * \param client Shared pointer to the pepClient.
@@ -219,8 +238,10 @@ rxcpp::observable<std::shared_ptr<Context>> createContext(const std::shared_ptr<
   ctx->force = values.has("force");
   ctx->resume = values.has("resume");
   ctx->updateFormat = values.has("update-pseudonym-format");
-  ctx->outputDirectory = values.get<std::filesystem::path>("output-directory").string();
-  ctx->tempDirectory = std::filesystem::path(ctx->outputDirectory).replace_filename(std::filesystem::path(ctx->outputDirectory).filename().string() + "-pending").string();
+  // Normalize, because deleting "path/." throws
+  auto outputDirectory = values.get<fs::path>("output-directory").lexically_normal();
+  ctx->outputDirectory = outputDirectory.string();
+  ctx->tempDirectory = AppendDirectoryNameSuffix(outputDirectory, "-pending").string();
   ctx->options.assumePristine = values.has("assume-pristine");
   ctx->allAccessible = values.has("all-accessible");
   ctx->exportFormats = ParseExportFormats(values.getOptionalMultiple<std::string>("export"));
@@ -349,11 +370,11 @@ std::shared_ptr<DownloadDirectory> createDownloadDirectory(const std::shared_ptr
   }
   else {
     // Either the outputDirectory and tempDirectory do not exist/are empty, or --force was specified. Either way, start with a clean slate.
-    if (std::filesystem::exists(ctx->outputDirectory)) {
-      std::filesystem::remove_all(ctx->outputDirectory);
+    if (fs::exists(ctx->outputDirectory)) {
+      fs::remove_all(ctx->outputDirectory);
     }
-    if (std::filesystem::exists(ctx->tempDirectory)) {
-      std::filesystem::remove_all(ctx->tempDirectory);
+    if (fs::exists(ctx->tempDirectory)) {
+      fs::remove_all(ctx->tempDirectory);
     }
     directory = DownloadDirectory::Create(ctx->tempDirectory, client, ctx->content, globalConfig, applyFileExtensions);
   }
@@ -364,7 +385,7 @@ void cleanUp(std::shared_ptr<Context> ctx) {
 
   //When updating, remove old data after succesfull pull
   if (ctx->update || ctx->updateFormat) {
-    std::filesystem::remove_all(ctx->outputDirectory);
+    fs::remove_all(ctx->outputDirectory);
 
     // The "rename" call below often hangs after our "remove_all", possibly due to file system latency.
     // (Letting the file system catch up while we) sleep reliably gets rid of the problem for me.
@@ -372,16 +393,16 @@ void cleanUp(std::shared_ptr<Context> ctx) {
   }
 
   //Move downloaded data to final destination after succesfull pull
-  if (std::filesystem::exists(ctx->outputDirectory)) {
+  if (fs::exists(ctx->outputDirectory)) {
     throw std::runtime_error("Output directory already exists, please remove it before initiating a pull.");
   }
-  std::filesystem::rename(ctx->tempDirectory, ctx->outputDirectory);
-  LOG(LOG_TAG, pep::info) << "Data downloaded to " << std::filesystem::absolute(ctx->outputDirectory).string();
+  fs::rename(ctx->tempDirectory, ctx->outputDirectory);
+  LOG(LOG_TAG, pep::info) << "Data downloaded to " << fs::absolute(ctx->outputDirectory).string();
 }
 
 struct ExportContext final {
   std::shared_ptr<pep::GlobalConfiguration> globalConfig;
-  std::filesystem::path input_directory;
+  fs::path input_directory;
   bool force;
 };
 
@@ -392,7 +413,7 @@ void ExecuteExports(const so::FormatFlags formats, const ExportContext ctx) {
   const auto table = so::TableFrom(*downloadDir);
   const auto exportAs = [&ctx](const std::string format, std::function<void(std::ofstream&)> write) {
     const auto dest = ctx.input_directory.parent_path() / ("export." + format); // assuming format == file extension
-    if (!ctx.force && std::filesystem::exists(dest)) {
+    if (!ctx.force && fs::exists(dest)) {
       LOG(LOG_TAG, pep::error) << "Export destination \"" + dest.string()
               + "\" already exists, please remove it and then run \"pepcli export " + format + "\".";
       return; // skip this format
@@ -430,7 +451,7 @@ protected:
     using pep::commandline::Parameter;
     using pep::commandline::Value;
     return ChildCommandOf<CliApplication>::getSupportedParameters()
-      + Parameter("output-directory", "Directory to write files to").shorthand('o').value(Value<std::filesystem::path>()
+      + Parameter("output-directory", "Directory to write files to").shorthand('o').value(Value<fs::path>()
         .directory().defaultsTo("pulled-data"))
       + Parameter("force", "Overwrite or remove existing data in output directory").shorthand('f')
       + Parameter("resume", "Resume a download from the temporary folder").shorthand('r')
