faac5d91df777f2b1d85f4722f8dffa29b702a34
Steven Wallis de Vries <swdv@cs.ru.nl>
2026-06-12 10:27:30 +0200

    Remove useless universal reference in TupleTail

--- cpp/pep/utils/MiscUtil.hpp
+++ cpp/pep/utils/MiscUtil.hpp
@@ -75,7 +75,7 @@ template<typename T>
 /// Strip the first element from a tuple
 template <typename Head, typename... Tail>
 [[nodiscard]] std::tuple<Tail...> TupleTail(std::tuple<Head, Tail...> tuple) {
-  return std::apply([](const auto&, auto&&... tail) {
+  return std::apply([](const auto&, auto&... tail) {
       return std::tuple{std::move(tail)...};
   }, std::move(tuple));
 }
