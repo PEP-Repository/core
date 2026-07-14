PEP-Repository/core

commit 18a3d413de2343c650fa2b2d04e323fa5a032b52

refac(Application): cleanup of `initializeLoggingOnce`

cpp/pep/application/Application.cpp
@@ -229,25 +229,17 @@ void Application::initializeLoggingOnce() {
   mShowVersionInfo = !values.has("suppress-version-info");
   std::vector<std::shared_ptr<Logging>> logging;
 
-  std::optional<severity_level> console_level;
-  if (values.has("loglevel")) {
-    console_level = Logging::ParseSeverityLevel(values.get<std::string>("loglevel"));
-  }
-  if (!console_level) {
-    console_level = consoleLogMinimumSeverityLevel();
-  }
-  if (console_level) {
+  if (auto console_level =
+          values.has("logLevel") ? values.getOptional<severity_level>("logLevel") : consoleLogMinimumSeverityLevel()) {
     logging.push_back(std::make_shared<ConsoleLogging>(*console_level));
     usingConsoleLog_ = true;
   }
 
-  auto file_level = fileLogMinimumSeverityLevel();
-  if (file_level) {
+  if (auto file_level = fileLogMinimumSeverityLevel()) {
     logging.push_back(std::make_shared<FileLogging>(*file_level));
   }
 
-  auto syslog_level = syslogLogMinimumSeverityLevel();
-  if (syslog_level) {
+  if (auto syslog_level = syslogLogMinimumSeverityLevel()) {
     logging.push_back(std::make_shared<SysLogging>(*syslog_level));
   }
 

commit 3423026a8fda0129955bf35d4b3792bb002e9f93

Fix `--loglevel` Broken by 18a3d413de2343c650fa2b2d04e323fa5a032b52

cpp/pep/application/Application.cpp
@@ -232,10 +232,8 @@ void Application::initializeLoggingOnce() {
   { // initialize logging sinks
     std::vector<std::shared_ptr<Logging>> logging;
 
-    if (auto console_level = values.has("logLevel")
-        ? values.getOptional<Severity>("logLevel")
-        : consoleLogMinimumSeverityLevel()) {
-      logging.push_back(std::make_shared<ConsoleLogging>(*console_level));
+    if (auto consoleLevelStr = values.getOptional<std::string>("loglevel")) {
+      logging.push_back(std::make_shared<ConsoleLogging>(Logging::ParseSeverity(*consoleLevelStr)));
       usingConsoleLog = true;
     }
 