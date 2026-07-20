b8be4004485a553297fe3d5eef1e8cff4edc8f0d
Kai van Lopik <K.vanLopik@cs.ru.nl>
2026-07-20 16:00:01 +0200

    Script now creates branch *before* trying to find a pipeline for it.

--- scripts/gitlab-pipeline.sh
+++ scripts/gitlab-pipeline.sh
@@ -52,11 +52,11 @@ trigger_and_wait() {
   gitlab_host=$("$SCRIPTPATH"/gitdir.sh origin-host "$git_dir")
   project_path=$("$SCRIPTPATH"/gitdir.sh origin-path "$git_dir")
 
-  pipeline_id=$(get_pipeline_id "$branchname") || return 1
-
   # GitLab can only run pipelines for branches (or tags): see https://stackoverflow.com/a/63460457
   >&2 echo "Creating branch by posting to Gitlab API at repository/branches?branch=$branchname&ref=$ref_sha"
   gitlab_api post "repository/branches?branch=$branchname&ref=$ref_sha"
+
+  pipeline_id=$(get_pipeline_id "$branchname") || return 1
 
   if [ -n "$pipeline_name" ]; then
     gitlab_api put "pipelines/$pipeline_id/metadata" --data "name=$pipeline_name" > /dev/null || true
