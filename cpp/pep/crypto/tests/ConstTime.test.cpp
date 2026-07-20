5dfd0e93353c24e4c1e2305b403c1d679ae4f655
Steven WdV <swdv@cs.ru.nl>
2026-07-20 12:10:08 +0200

    Use pep::TestError in ConstTime tests

--- cpp/pep/crypto/tests/ConstTime.test.cpp
+++ cpp/pep/crypto/tests/ConstTime.test.cpp
@@ -1,5 +1,7 @@
 #include <pep/crypto/ConstTime.hpp>
 
+#include <pep/utils/TestError.hpp>
+
 #include <algorithm>
 #include <array>
 
@@ -9,16 +11,11 @@ using namespace std::ranges;
 
 namespace {
 
-//TODO Use class from https://gitlab.pep.cs.ru.nl/pep/core/-/merge_requests/2211/diffs?commit_id=2ed021e4e20832b74aa4fd72ea8e7ba98f10b2d2#fa2a04b1b3dba5ead10100f492690a0b1a03c6c4
-struct TestError : std::exception {
-  [[nodiscard]] const char* what() const noexcept override { return "TestError"; }
-};
-
 constexpr auto LazyThrowingIota =
   views::iota(0, 10)
   | views::transform([](int i) {
     if (i > 5) {
-      throw TestError{};
+      throw pep::TestError{};
     }
     return i;
   });
@@ -28,7 +25,7 @@ TEST(ConstTime, IsZero) {
   EXPECT_FALSE(pep::const_time::IsZero(std::array{0, 0b11, 0}));
 
   // Make sure IsZero does not use short-circuit logic
-  EXPECT_THROW((void) pep::const_time::IsZero(LazyThrowingIota), TestError);
+  EXPECT_THROW((void) pep::const_time::IsZero(LazyThrowingIota), pep::TestError);
   // STL for comparison
   EXPECT_NO_THROW((void) all_of(LazyThrowingIota, std::bind_front(std::equal_to{}, 0)));
 }
@@ -43,7 +40,7 @@ TEST(ConstTime, IsEqual) {
   EXPECT_FALSE(pep::const_time::IsEqual(std::array{0, 1}, std::array{0, 1, 2}));
 
   // Make sure IsEqual does not use short-circuit logic
-  EXPECT_THROW((void) pep::const_time::IsEqual(LazyThrowingIota, views::iota(10, 20)), TestError);
+  EXPECT_THROW((void) pep::const_time::IsEqual(LazyThrowingIota, views::iota(10, 20)), pep::TestError);
   // STL for comparison
   EXPECT_NO_THROW((void) equal(LazyThrowingIota, views::iota(10, 20)));
 }
