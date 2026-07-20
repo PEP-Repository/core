f759af5b0e7744c17f28eaee8490f27f5e6d6ed6
Steven WdV <swdv@cs.ru.nl>
2026-07-20 16:13:52 +0200

    Replace ASSERT in MapUtils tests by EXPECT

--- cpp/pep/utils/tests/MapUtils.test.cpp
+++ cpp/pep/utils/tests/MapUtils.test.cpp
@@ -5,38 +5,38 @@ namespace {
 
 TEST(MapUtils, IsSubset) {
   // empty set is subset of every other set
-  ASSERT_TRUE(pep::IsSubset(std::vector<int>{}, std::vector<int>{}));
-  ASSERT_TRUE(pep::IsSubset(std::vector<int>{}, std::vector{1}));
+  EXPECT_TRUE(pep::IsSubset(std::vector<int>{}, std::vector<int>{}));
+  EXPECT_TRUE(pep::IsSubset(std::vector<int>{}, std::vector{1}));
 
-  ASSERT_TRUE(pep::IsSubset(std::vector{1}, std::vector{1}));
-  ASSERT_TRUE(pep::IsSubset(std::vector{1}, std::vector{1, 2}));
-  ASSERT_TRUE(pep::IsSubset(std::vector{1, 2}, std::vector{1, 2, 3}));
+  EXPECT_TRUE(pep::IsSubset(std::vector{1}, std::vector{1}));
+  EXPECT_TRUE(pep::IsSubset(std::vector{1}, std::vector{1, 2}));
+  EXPECT_TRUE(pep::IsSubset(std::vector{1, 2}, std::vector{1, 2, 3}));
 
   // unsorted
-  ASSERT_TRUE(pep::IsSubset(std::vector{2, 1}, std::vector{ 2, 3, 1}));
+  EXPECT_TRUE(pep::IsSubset(std::vector{2, 1}, std::vector{ 2, 3, 1}));
 
   // not a subset
-  ASSERT_FALSE(pep::IsSubset(std::vector{1}, std::vector{2}));
-  ASSERT_FALSE(pep::IsSubset(std::vector{1, 2}, std::vector{2}));
+  EXPECT_FALSE(pep::IsSubset(std::vector{1}, std::vector{2}));
+  EXPECT_FALSE(pep::IsSubset(std::vector{1, 2}, std::vector{2}));
 }
 
 TEST(MapUtils, TryFindDuplicateValue) {
-  ASSERT_EQ(pep::TryFindDuplicateValue(std::vector<int>{}), std::nullopt);
-  ASSERT_EQ(pep::TryFindDuplicateValue(std::vector<int>{1 }), std::nullopt);
-  ASSERT_EQ(pep::TryFindDuplicateValue(std::vector<int>{1, 1}), 1);
-  ASSERT_EQ(pep::TryFindDuplicateValue(std::vector<int>{1, 2, 1}), 1);
-  ASSERT_EQ(pep::TryFindDuplicateValue(std::vector<int>{1, 2, 2,1}), 1);
+  EXPECT_EQ(pep::TryFindDuplicateValue(std::vector<int>{}), std::nullopt);
+  EXPECT_EQ(pep::TryFindDuplicateValue(std::vector<int>{1 }), std::nullopt);
+  EXPECT_EQ(pep::TryFindDuplicateValue(std::vector<int>{1, 1}), 1);
+  EXPECT_EQ(pep::TryFindDuplicateValue(std::vector<int>{1, 2, 1}), 1);
+  EXPECT_EQ(pep::TryFindDuplicateValue(std::vector<int>{1, 2, 2,1}), 1);
 }
 
 TEST(MapUtils, ContainsUniqueValues) {
-  ASSERT_EQ(pep::ContainsUniqueValues(std::vector<int>{}), true);
-  ASSERT_EQ(pep::ContainsUniqueValues(std::vector<int>{1 }), true);
-  ASSERT_EQ(pep::ContainsUniqueValues(std::vector<int>{1, 2}), true);
+  EXPECT_EQ(pep::ContainsUniqueValues(std::vector<int>{}), true);
+  EXPECT_EQ(pep::ContainsUniqueValues(std::vector<int>{1 }), true);
+  EXPECT_EQ(pep::ContainsUniqueValues(std::vector<int>{1, 2}), true);
 
-  ASSERT_EQ(pep::ContainsUniqueValues(std::vector<int>{1, 1}), false);
-  ASSERT_EQ(pep::ContainsUniqueValues(std::vector<int>{1, 1, 2}), false);
-  ASSERT_EQ(pep::ContainsUniqueValues(std::vector<int>{1, 2, 1}), false);
-  ASSERT_EQ(pep::ContainsUniqueValues(std::vector<int>{1, 2, 2,1}), false);
+  EXPECT_EQ(pep::ContainsUniqueValues(std::vector<int>{1, 1}), false);
+  EXPECT_EQ(pep::ContainsUniqueValues(std::vector<int>{1, 1, 2}), false);
+  EXPECT_EQ(pep::ContainsUniqueValues(std::vector<int>{1, 2, 1}), false);
+  EXPECT_EQ(pep::ContainsUniqueValues(std::vector<int>{1, 2, 2,1}), false);
 }
 
 TEST(InsertNonDuplicates, AllowsNonDuplicatesAndRejectsDuplicates) {
