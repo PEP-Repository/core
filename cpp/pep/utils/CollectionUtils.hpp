31b7a8f802afe59716a794b866eda12971019c5c
Linus van Elswijk <l.vanelswijk@science.ru.nl>
2026-08-06 16:27:53 +0200

    refac(utils): TryFindIntersect -> TryFindCommondValue

--- cpp/pep/utils/CollectionUtils.hpp
+++ cpp/pep/utils/CollectionUtils.hpp
@@ -25,7 +25,7 @@ using QualifiedRangeValue = std::remove_reference_t<std::ranges::range_reference
 /// Returns a value that's included in both vectors, or std::nullopt if no such value exists.
 /// Equality is determined by the specified Compare object.
 template <typename T, typename TCompare>
-std::optional<T> TryFindIntersect(std::vector<T> vecA, std::vector<T> vecB, const TCompare& comp) {
+std::optional<T> TryFindCommonValue(std::vector<T> vecA, std::vector<T> vecB, const TCompare& comp) {
   std::sort(vecA.begin(), vecA.end(), comp);
   std::sort(vecB.begin(), vecB.end(), comp);
   std::vector<T> intersect;
@@ -37,8 +37,8 @@ std::optional<T> TryFindIntersect(std::vector<T> vecA, std::vector<T> vecB, cons
 /// Returns a value that's included in both vectors, or std::nullopt if no such value exists.
 /// Equality is determined by a default-constructed instance of the specified Compare type.
 template <typename T, typename TCompare = std::less<T>>
-std::optional<T> TryFindIntersect(std::vector<T> vecA, std::vector<T> vecB) {
-  return TryFindIntersect(vecA, vecB, TCompare());
+std::optional<T> TryFindCommonValue(std::vector<T> vecA, std::vector<T> vecB) {
+  return TryFindCommonValue(vecA, vecB, TCompare());
 }
 
 /// \brief Fills a destination range with strings from a source range without exceeding the specified destination capacity.
