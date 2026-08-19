3ea3ffb3272561f3c4b2bdc4415ec2f0bdfe9fbe
Kai van Lopik <K.vanLopik@cs.ru.nl>
2026-08-19 14:07:47 +0200

    Derived classes are now hard-coded into StudyAspectPuller factory method instead of relying on the (failing) SelfRegistering<> mechanism.

--- cpp/pep/pullcastor/StudyAspectPuller.hpp
+++ cpp/pep/pullcastor/StudyAspectPuller.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <pep/pullcastor/CastorParticipant.hpp>
-#include <pep/utils/SelfRegistering.hpp>
 
 namespace pep {
 namespace castor {
@@ -10,44 +9,14 @@ namespace castor {
   * \brief (Base class for classes that) pulls Castor data for a specific study aspect, e.g. "SURVEYs for study XYZ".
   */
 class StudyAspectPuller : public std::enable_shared_from_this<StudyAspectPuller>, boost::noncopyable {
-  template <typename TDerived, typename TRegistrar, bool>
-  friend class pep::SelfRegistering;
 
 private:
   std::shared_ptr<StudyPuller> mStudy;
   std::string mSpColumn;
   std::string mColumnNamePrefix;
 
-  // A function that can create a (shared_ptr to) StudyAspectPuller. Such functions are statically registered by derived TypedStudyAspectPuller<> class.
-  using CreateFunction = std::function<std::shared_ptr<StudyAspectPuller>(std::shared_ptr<StudyPuller>, const StudyAspect&)>;
-
-  // Prevent trouble w.r.t. static initialization order by wrapping our (static) list of CreateFunction in a function.
-  static std::unordered_map<CastorStudyType, CreateFunction>& GetCreateFunctions();
-
 protected:
   StudyAspectPuller(std::shared_ptr<StudyPuller> study, const StudyAspect& aspect);
-
-  /*!
-  * \brief Registers the specified TDerived type as the handler for the import of its specified aspect STUDY_TYPE.
-  * \tparam TDerived The derived type that imports the specified STUDY_TYPE.
-  * \return The STUDY_TYPE specified by TDerived.
-  */
-  template <typename TDerived>
-  static CastorStudyType RegisterType() {
-    static_assert(std::is_base_of<StudyAspectPuller, TDerived>::value);
-
-    // Raw &TDerived::Create is (too) difficult to cast to a CreateFunction, so we wrap it in a lambda
-    CreateFunction createDerived = [](std::shared_ptr<StudyPuller> study, const StudyAspect& aspect) {
-      return TDerived::Create(study, aspect);
-    };
-
-    auto& registered = GetCreateFunctions();
-    if (!registered.emplace(std::make_pair(TDerived::STUDY_TYPE, createDerived)).second) {
-      throw std::runtime_error("Duplicate registration for study aspect puller type for Castor study type " + std::to_string(TDerived::STUDY_TYPE));
-    }
-
-    return TDerived::STUDY_TYPE;
-  }
 
   /*!
   * \brief Produces the prefix to use for column names when importing data for this study aspect.
@@ -83,20 +52,6 @@ public:
   * \return The name of a PEP column that stores short pseudonym values.
   */
   inline const std::string& getShortPseudonymColumn() const noexcept { return mSpColumn; }
-};
-
-/*!
-  * \brief Helper (base) class that automatically registers the TDerived aspect puller type as the handler for the specified study aspect TYPE.
-  */
-template <typename TDerived, CastorStudyType TYPE, bool REGISTER = true>
-class TypedStudyAspectPuller : public StudyAspectPuller, public SelfRegistering<TDerived, StudyAspectPuller, REGISTER> {
-public:
-  static constexpr CastorStudyType STUDY_TYPE = TYPE;
-
-protected:
-  inline TypedStudyAspectPuller(std::shared_ptr<StudyPuller> study, const StudyAspect& aspect)
-    : StudyAspectPuller(study, aspect) {
-  }
 };
 
 }
