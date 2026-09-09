8e3598e6ab450f52b3b2d5b50e2f5b2525b1b938
Kai van Lopik <K.vanLopik@cs.ru.nl>
2026-09-09 14:01:33 +0200

    Improved diagnostic output.

--- cpp/pep/accessmanager/tests/AccessManagerMessages.test.cpp
+++ cpp/pep/accessmanager/tests/AccessManagerMessages.test.cpp
@@ -2,8 +2,8 @@
 #include <gtest/gtest.h>
 
 TEST(AccessManagerMessages, AliasNamesNormalizeToAliasedTypes) {
-  static_assert(std::is_same_v<pep::ColumnAccess, pep::ColumnAccessResponse>, "One of these types aliases the other");
+  static_assert(std::is_same_v<pep::ColumnAccess, pep::ColumnAccessResponse>, "One of these types must alias the other");
 
-  EXPECT_EQ(pep::GetNormalizedTypeName<pep::ColumnAccess>(), "ColumnAccess") << "The ColumnAccess type is defined (as opposed to aliased)";
+  EXPECT_EQ(pep::GetNormalizedTypeName<pep::ColumnAccess>(), "ColumnAccess") << "The ColumnAccess type must be defined (as opposed to aliased)";
   EXPECT_EQ(pep::GetNormalizedTypeName<pep::ColumnAccessResponse>(), pep::GetNormalizedTypeName<pep::ColumnAccess>()) << "Alias and aliased type do not normalize to the same name";
 }
